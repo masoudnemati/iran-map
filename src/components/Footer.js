fce2149104f06c4fd64c807b3c871c85db2f5cf6
funXshow <funXshow4@gmail.com>
2021-10-08 15:09:48 +0330

    added link to github

---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <>
+    <a href="https://github.com/masoudnemati/iran-map"><img loading="lazy" width="149" height="149" src="https://github.blog/wp-content/uploads/2008/12/forkme_left_green_007200.png?resize=149%2C149" className="attachment-full size-full" alt="Fork me on GitHub" data-recalc-dims="1"/></a>
+
+    <style jsx>{`
+        
+        a {
+            position: fixed;
+            top: 0;
+            left: 0;
+        }
+
+        `}</style>
+        </>
+    )
+}
+
+export default Footer
